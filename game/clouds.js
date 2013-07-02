8d1553b3302ad659cf898c26d93ea93369302623
Jim Counts <jamesrcounts@outlook.com>
2013-07-01 21:07:25 -0700

    move clouds to file

---
+++ game/clouds.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+var clouds = (function (spec) {
+    var self = [];
+    self.count = 10;
+
+    self.reset = function () {
+        for (var j = 0; j < self.count; j++) {
+            self[j] = [Math.random() * spec.width,
+                Math.random() * spec.height,
+                Math.random() * 100,
+                Math.random() / 2];
+        }
+    };
+
+    self.draw = function () {
+        var ctx = spec.context();
+        for (var i = 0; i < this.count; i++) {
+            ctx.fillStyle = 'rgba(255, 255, 255, ' + this[i][3] + ')';
+            ctx.beginPath();
+            ctx.arc(this[i][0], this[i][1], this[i][2], 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fill();
+        }
+    };
+
+    self.update = function (deltaY) {
+        for (var i = 0; i < this.count; i++) {
+            if (this[i][1] - this[i][2] <= spec.height) {
+                this[i][1] += deltaY;
+            } else {
+                this[i][0] = Math.random() * spec.width;
+                this[i][2] = Math.random() * 100;
+                this[i][1] = 0 - this[i][2];
+                this[i][3] = Math.random() / 2;
+            }
+        }
+    };
+
+    self.reset();
+    return self;
+})(board);
--- game/game.js
+++ game/game.js
@@ -1,6 +1,5 @@
 ﻿var startGame;
 var checkCollisions;
-var clouds;
 var control;
 var endGame;
 var gameLoop;
@@ -20,46 +19,6 @@ player.checkEndGame = function() {
     }
 };
 
-clouds = (function(spec) {
-    var self = [];
-    self.count = 10;
-
-    self.reset = function() {
-        for (var j = 0; j < self.count; j++) {
-            self[j] = [Math.random() * spec.width,
-                Math.random() * spec.height,
-                Math.random() * 100,
-                Math.random() / 2];
-        }
-    };
-
-    self.draw = function() {
-        var ctx = spec.context();
-        for (var i = 0; i < this.count; i++) {
-            ctx.fillStyle = 'rgba(255, 255, 255, ' + this[i][3] + ')';
-            ctx.beginPath();
-            ctx.arc(this[i][0], this[i][1], this[i][2], 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.fill();
-        }
-    };
-
-    self.update = function(deltaY) {
-        for (var i = 0; i < this.count; i++) {
-            if (this[i][1] - this[i][2] <= spec.height) {
-                this[i][1] += deltaY;
-            } else {
-                this[i][0] = Math.random() * spec.width;
-                this[i][2] = Math.random() * 100;
-                this[i][1] = 0 - this[i][2];
-                this[i][3] = Math.random() / 2;
-            }
-        }
-    };
-
-    self.reset();
-    return self;
-})(board);
 
 platforms = (function(spec) {
     var self = [];
