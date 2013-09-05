4649512ce9c2cf28c2edb62ade64242feb92deee
Jim Counts <jamesrcounts@outlook.com>
2013-09-04 20:43:29 -0700

    data collection

---
+++ game/src/Main.js
@@ -0,0 +1,75 @@
+﻿/*jshint bitwise: false*/
+var global = function () {
+    "use strict";
+
+    var self = {};
+
+    var setSessionCookie = function (name, value) {
+        var cookie = name + "=" + value;
+        document.cookie = cookie;
+        return cookie;
+    };
+
+    self.parseCookie = function () {
+        var cookie = {};
+        var crumbs = document.cookie.split(';');
+        var crumbsCount = crumbs.length;
+        for (var i = 0; i < crumbsCount; i++) {
+            var pair = crumbs[i].split('=');
+            var key = pair[0] || "";
+            if (key !== "") {
+                cookie[key.trim()] = pair[1];
+            }
+        }
+        return cookie;
+    };
+
+    var getSessionFromCookie = function () {
+        var cookie = self.parseCookie();
+        return cookie.sessionid;
+    };
+
+    var getSession = function () {
+        if (!self.sessionid) {
+            self.sessionid = getSessionFromCookie();
+            if (!self.sessionid) {
+                self.sessionid = self.uuid();
+                setSessionCookie("sessionid", self.sessionid);
+            }
+        }
+        return self.sessionid;
+    };
+
+    self.uuid = function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0;
+            var v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    };
+
+    self.getDate = function () {
+        return Date();
+    };
+
+    self.collectDataAsync = function ($category, $label, $value) {
+        var result = {
+            partitionKey: getSession(),
+            createDate: self.getDate(),
+            category: $category,
+            label: $label,
+            value: $value,
+        };
+
+        $.ajax(
+            "http://localhost:7820/Collect", {
+                data: result,
+                dataType: 'jsonp',
+                contentType: 'application/json; charset=utf-8',
+            });
+
+        return result;
+    };
+
+    return self;
+};
