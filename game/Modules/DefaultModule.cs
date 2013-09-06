52b362a6602f83ed0a89d628ea4cbe96fddb5093
Jim Counts <jamesrcounts@outlook.com>
2013-09-06 07:23:51 -0700

    Setting up Azure storage

--- game/Modules/DefaultModule.cs
+++ game/Modules/DefaultModule.cs
@@ -9,7 +9,28 @@
 
 namespace Game.Modules
 {
+    using System;
+    using System.Configuration;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Table;
     using Nancy;
+    using Nancy.ModelBinding;
+
+    public class DataPoint : TableEntity
+    {
+        public DataPoint()
+        {
+            this.RowKey = Guid.NewGuid().ToString();
+        }
+
+        public string Category { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string Label { get; set; }
+
+        public string Value { get; set; }
+    }
 
     /// <summary>
     /// Hosts the server side code.
@@ -22,6 +43,21 @@ namespace Game.Modules
         public DefaultModule()
         {
             this.Get["/"] = _ => View["index"];
+            var cs = ConfigurationManager.AppSettings["StorageConnectionString"];
+            var table = CloudStorageAccount.Parse(cs)
+                                           .CreateCloudTableClient()
+                                           .GetTableReference("DataPoints");
+            this.Get["/Collect"] = _ => this.SaveTo(table);
+            this.Post["/Collect"] = _ => this.SaveTo(table);
+        }
+
+        private Response SaveTo(CloudTable table)
+        {
+            var dataPoint = this.Bind<DataPoint>();
+            var operation = TableOperation.Insert(dataPoint);
+            table.Execute(operation);
+
+            return Response.AsJson(dataPoint);
         }
     }
 }
