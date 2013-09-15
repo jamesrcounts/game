f42378ccb771f91d008d2f25d840b6d2508e48f9
Jim Counts <jamesrcounts@outlook.com>
2013-09-15 11:18:38 -0700

    requiejs and jasmine working together

---
+++ game/spec/dataSpec.js
@@ -0,0 +1,58 @@
+define(["src/data"], function (data) {
+    var expected = {
+        partitionKey: "fa404b90-2b8a-41e4-a3c6-e232bb7c9bfa",
+        createDate: "2013-09-02T22:31:41.918Z",
+        category: "Platforms",
+        label: "Bounce",
+        value: "flubber"
+    };
+
+    describe("the data model", function () {
+        it("should generate uuids", function () {
+            var uuid = data.uuid();
+            expect(uuid).toMatch(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i);
+        });
+
+        describe("the collected data", function () {
+            beforeEach(function () {
+                document.cookie = "sessionid= ;expires=Sat Aug 31 2013 20:56:22 GMT-0700 (Pacific Daylight Time)";
+                delete data.sessionid;
+                data.uuid = function () { return "fa404b90-2b8a-41e4-a3c6-e232bb7c9bfa"; };
+                data.getDate = function () {
+                    return new Date('2013-09-02T22:31:41.918Z').toJSON();
+                };
+            });
+
+            it("should get the cached session id when available", function () {
+                data.sessionid = "fa404b90-2b8a-41e4-a3c6-e232bb7c9bfa";
+                var result = data.collectDataAsync(
+                    "Platforms",
+                    "Bounce",
+                    "flubber");
+                expect(result).toEqual(expected);
+            });
+
+            it("should get session id from cookie when available", function () {
+                document.cookie = "sessionid=fa404b90-2b8a-41e4-a3c6-e232bb7c9bfa";
+                var result = data.collectDataAsync(
+                    "Platforms",
+                    "Bounce",
+                    "flubber");
+
+                expect(result).toEqual(expected);
+                expect(data.sessionid).toEqual("fa404b90-2b8a-41e4-a3c6-e232bb7c9bfa");
+            });
+
+            it("should get the session id from global.uuid when needed", function () {
+                var result = data.collectDataAsync(
+                    "Platforms",
+                    "Bounce",
+                    "flubber");
+                expect(result)
+                    .toEqual(expected);
+                expect(data.parseCookie().sessionid)
+                    .toEqual("fa404b90-2b8a-41e4-a3c6-e232bb7c9bfa");
+            });
+        });
+    });
+});
---
+++ game/spec/specRunner.js
@@ -0,0 +1,32 @@
+﻿require.config({
+    paths: {
+        jquery: "../lib/jquery-1.10.1/jquery-1.10.1",
+        jasmine: "../lib/jasmine-1.3.1/jasmine",
+        'jasmine-html': "../lib/jasmine-1.3.1/jasmine-html",
+        src: "../src",
+    },
+    shim: {
+        jasmine: { exports: 'jasmine' },
+        'jasmine-html': { deps: ['jasmine'], exports: 'jasmine' }
+    }
+});
+
+require(["jquery", "jasmine-html"], function ($, jasmine) {
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 1000;
+
+    var htmlReporter = new jasmine.HtmlReporter();
+
+    jasmineEnv.addReporter(htmlReporter);
+
+    jasmineEnv.specFilter = function (spec) {
+        return htmlReporter.specFilter(spec);
+    };
+
+    var specs = ['dataSpec'];
+    $(function () {
+        require(specs, function (spec) {
+            jasmineEnv.execute();
+        });
+    });
+});
