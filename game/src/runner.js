eb194b3dd8587cd53ca9f847ab0443df2ff4db4d
Jim Counts <jamesrcounts@outlook.com>
2013-09-21 09:18:55 -0700

    little bit of require js

--- game/src/runner.js
+++ game/src/runner.js
@@ -2,12 +2,14 @@
     paths: {
         easeljs: "../lib/easeljs-0.6.0/easeljs-0.6.0.min",
         jquery: "../lib/jquery-1.10.1/jquery-1.10.1",
+        bootstrap: "../lib/bootstrap/bootstrap",
     },
     shim: {
         easeljs: { exports: 'createjs' },
+        bootstrap: { deps: ["jquery"] },
     }
 });
 
-require(["game"], function (game) {
+require(["jquery", "game"], function (_, game) {
     game.start();
 });
