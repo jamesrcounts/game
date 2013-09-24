ccf672461ec066b782215367fcac0128e480838d
Jim Counts <jamesrcounts@outlook.com>
2013-09-23 21:32:54 -0700

    tested fully populated settings

--- game/spec/gameSpec.js
+++ game/spec/gameSpec.js
@@ -1,4 +1,4 @@
-﻿define(["game"], function (game) {
+﻿define(["game", "player", "platforms"], function (game, player, platforms) {
     describe("game settings", function () {
         it("looks like this at startup", function () {
             expect(JSON.stringify(game.getSettings()))
@@ -6,8 +6,10 @@
         });
 
         it("looks like this when completly populated", function () {
+            player.agility("quickly");
+            platforms.grouping("anywhere");
             expect(JSON.stringify(game.getSettings()))
-                .toEqual('{"board":{"width":320},"platforms":{"bounce":2,"count":7,"move":false,"grouping":foo},"player":{"agility":foo}}');
+                .toEqual('{"board":{"width":320},"platforms":{"bounce":2,"count":7,"move":false,"grouping":"anywhere"},"player":{"agility":3}}');
         });
     });
 });
